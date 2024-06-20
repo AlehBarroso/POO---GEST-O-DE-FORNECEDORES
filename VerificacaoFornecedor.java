92dd81cf6e1d5444d6144989de0f793b20979d7e
Lucas <uchoa5302@gmail.com>
2024-06-19 23:47:05 -0400

    Add files via upload

---
+++ VerificacaoFornecedor.java
@@ -0,0 +1,33 @@
+package VerificacaoFornecedor;
+
+public class VerificacaoFornecedor {
+
+
+    public static void main(String[] args) {
+	        String nomeFornecedor = "FornecedorD"; 
+	
+	        
+	        boolean fornecedorEncontrado = verificarFornecedor(nomeFornecedor);
+	
+	
+	        if (!fornecedorEncontrado) {
+	            System.out.println("Status HTTP 404 (Not Found): Fornecedor não encontrado.");
+	        } else {
+	            System.out.println("Fornecedor encontrado.");
+	 }
+	}
+
+	public static boolean verificarFornecedor(String nomeFornecedor) {
+
+        String[] fornecedoresExistentes = {"FornecedorA", "FornecedorB", "FornecedorC"};
+
+
+        for (String fornecedor : fornecedoresExistentes) {
+            if (fornecedor.equals(nomeFornecedor)) {
+                return true; 
+            }
+        }
+
+        return false; 
+    }
+}
