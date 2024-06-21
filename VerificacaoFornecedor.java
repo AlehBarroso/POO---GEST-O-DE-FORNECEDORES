68d9c375c4b67b173d076ea4e9ee736e914aa139
Lucas <uchoa5302@gmail.com>
2024-06-20 23:56:38 -0400

    Add files via upload

--- VerificacaoFornecedor.java
+++ VerificacaoFornecedor.java
@@ -1,33 +1,28 @@
-package VerificacaoFornecedor;
+package com.lucas.fornecedores;
 
 public class VerificacaoFornecedor {
 
-
     public static void main(String[] args) {
-	        String nomeFornecedor = "FornecedorD"; 
-	
-	        
-	        boolean fornecedorEncontrado = verificarFornecedor(nomeFornecedor);
-	
-	
-	        if (!fornecedorEncontrado) {
-	            System.out.println("Status HTTP 404 (Not Found): Fornecedor não encontrado.");
-	        } else {
-	            System.out.println("Fornecedor encontrado.");
-	 }
-	}
+        String nomeFornecedor = "FornecedorD";
+        
+        boolean fornecedorEncontrado = verificarFornecedor(nomeFornecedor);
 
-	public static boolean verificarFornecedor(String nomeFornecedor) {
+        if (!fornecedorEncontrado) {
+            System.out.println("Status HTTP 404 (Not Found): Fornecedor não encontrado.");
+        } else {
+            System.out.println("Fornecedor encontrado.");
+        }
+    }
 
+    public static boolean verificarFornecedor(String nomeFornecedor) {
         String[] fornecedoresExistentes = {"FornecedorA", "FornecedorB", "FornecedorC"};
-
 
         for (String fornecedor : fornecedoresExistentes) {
             if (fornecedor.equals(nomeFornecedor)) {
-                return true; 
+                return true;
             }
         }
 
-        return false; 
+        return false;
     }
 }
